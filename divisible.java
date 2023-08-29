2bb6ae90a7bc6d6b9503a39772d4f02488eac49c
saiteja2712 <gurramsaiteja2000@gmail.com>
2023-08-29 16:00:03 +0530

    secoond file

---
+++ divisible.java
@@ -0,0 +1,26 @@
+class Main
+{
+public static void main(String args[])
+{
+int n=27;
+if(n%5==0)
+{
+if(n%3==0)
+{
+System.out.println("Fizzbuzz");
+}
+}
+if(n%5==0)
+{
+System.out.println("buzz");
+}
+if(n%3==0)
+{
+System.out.println("Fizz");
+}
+else
+{
+System.out.println("not");
+}
+}
+}
